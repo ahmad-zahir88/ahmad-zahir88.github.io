0a413fc1c04438075210b7a84c1f8402cc61febe
ahmad-zahir88 <91833149+ahmad-zahir88@users.noreply.github.com>
2023-10-20 06:24:37 +1100

    Created curriculum vitae component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { CurriculumVitaeComponent } from './curriculum-vitae/curriculum-vitae.component';
 
 
 
 @NgModule({
   declarations: [
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    CurriculumVitaeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    CurriculumVitaeComponent
   ]
 })
 export class ComponentsModule { }
